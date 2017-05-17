bc02545c2f2b21b6bd17788cf93930daff65e572
Olliver Schinagl <o.schinagl@ultimaker.com>
2017-05-17 10:55:57 +0200

    buildscript: Build the flask-restplus package
    The buildscript now builds a debian package of the flask-restplus
    package.
    
    CL-46
    
    Signed-off-by: Olliver Schinagl <o.schinagl@ultimaker.com>

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+SRC_PATH="$(pwd)"
+MODULE_PATH="$(pwd)/flask-restplus"
+
 init() {
 	git submodule update --init --recursive
 }
 
+build() {
+	cd "${MODULE_PATH}" &&
+	python3 setup.py --command-packages=stdeb.command bdist_deb &&
+	cp deb_dist/python3-flask-restplus_*.deb "${SRC_PATH}/"
+	cd "${SRC_PATH}"
+}
+
 init
+build
